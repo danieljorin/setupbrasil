89ff6cd64e46d2fb0a1de3546d8ce2207682d68d
danieljorin <danieljorin1212@gmail.com>
2026-05-17 03:36:36 -0300

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -12,7 +12,7 @@ export async function middleware(request: NextRequest) {
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL || '',
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
-      {sssss
+      {
         cookies: {
           getAll() {
             return request.cookies.getAll();
